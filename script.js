2d4d6547668b95a35a4d8f3a15b8b174d38fe286
John Morrow <CodeMorrow@yahoo.com>
2016-07-28 14:07:18 -0500

    Working

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ window.addEventListener("load", function() {
 
 		var currentWeather = document.getElementById("currentWeather");
 		var currentTemp = document.getElementById("currentTemp");
+		var todayHighTemp = document.getElementById("todayHighTemp");
+		var todayLowTemp = document.getElementById("todayLowTemp");
 		var dailySummary = document.getElementById("dailySummary");
 		var sunrise = document.getElementById("sunrise");
 		var sunset = document.getElementById("sunset");
@@ -21,6 +23,58 @@ window.addEventListener("load", function() {
 		var pressure = document.getElementById("pressure");
 		var visibility = document.getElementById("visibility");
 
-		
+
+		currentWeather.innerHTML = response.currently.summary;
+		currentTemp.innerHTML = Math.round(response.currently.apparentTemperature) + "&deg;";
+		todayHighTemp.innerHTML = Math.round(response.daily.data[0].temperatureMax) + "&deg;";
+		todayLowTemp.innerHTML = Math.round(response.daily.data[0].temperatureMin) + "&deg;";
+		dailySummary.innerHTML = "Today: " + response.daily.summary;
+		sunrise.innerHTML = convertTime(response.daily.data[0].sunriseTime);
+		sunset.innerHTML = convertTime(response.daily.data[0].sunsetTime);
+		chanceOfRain.innerHTML = response.daily.data[0].precipProbability + "%";
+		humidity.innerHTML = response.daily.data[0].humidity * 100 + "%";
+		wind.innerHTML = Math.round(response.daily.data[0].windSpeed) + " mph " + degToCompass(response.daily.data[0].windBearing);
+		realTemp.innerHTML = Math.round(response.daily.data[0].apparentTemperatureMax) + "&deg;";
+		precipitation.innerHTML = response.daily.data[0].precipIntensity + " in";
+		pressure.innerHTML = Math.round(29.92 * response.daily.data[0].pressure / 1013.25) + " in";
+		visibility.innerHTML = response.daily.data[0].visibility + " mi";
+		populateHourlyTemps(response.hourly.data);
+		populateDailyHighs(response.daily.data);
+		populateDailyLows(response.daily.data);
+
+		function convertTime (unixTime){
+	    	var date = new Date(unixTime*1000);
+			var hours = date.getHours();
+			var minutes = "0" + date.getMinutes();
+			return hours + ':' + minutes.substr(-2);
+	    };
+
+	    function populateHourlyTemps (hourlyData){
+	    	var tempArr = document.getElementsByClassName("hourlyTemp");
+	    	for (i = 0; i < tempArr.length; i++){
+	    		tempArr[i].innerHTML = Math.round(hourlyData[i+7].temperature) + "&deg;";
+	    	}
+	    };
+
+	  	function populateDailyHighs (highData){
+	    	var highArr = document.getElementsByClassName("dayHighTemp");
+	    	for (i = 0; i < highArr.length; i++){
+	    		highArr[i].innerHTML = Math.round(highData[i].temperatureMax) + "&deg;";
+	    	}
+	    };
+
+		function populateDailyLows (lowData){
+	    	var lowArr = document.getElementsByClassName("dayLowTemp");
+	    	for (i = 0; i < lowArr.length; i++){
+	    		lowArr[i].innerHTML = Math.round(lowData[i].temperatureMin) + "&deg;";
+	    	}
+	    };  
+
+	    function degToCompass(num) {
+            var val = Math.floor((num / 45) + 0.5);
+            var arr = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
+            return arr[(val % 8)];
+        };
+
 	});
 });
